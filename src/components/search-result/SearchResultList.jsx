56682b0505c869c9ea0eebe150ee067bc631753b
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-04-30 19:19:50 +0530

    added search highlighting

--- src/components/search-result/SearchResultList.jsx
+++ src/components/search-result/SearchResultList.jsx
@@ -6,14 +6,29 @@ import "./searc-result.css";
 function SearchResultList({ names, query }) {
     const filteredData = names?.filter((name) => name.toLowerCase().includes(query));
 
+    //highlight & render filtered names
     const renderNames = () => {
-        return filteredData?.map((name, index) => {
+        return filteredData?.map((name, i) => {
+            //finding index of substring(q) in name
+            const indexOfQueryInName = name.indexOf(query);
+
+            //extracting beginning of text   when highlighted part is in the middle of text
+            const start = name.slice(0, indexOfQueryInName);
+
+            const match = name.slice(indexOfQueryInName, indexOfQueryInName + query.length);
+
+            //extracting remaining portion of text after highlighted part
+            const end = name.slice(indexOfQueryInName + query.length);
             return (
                 <div
-                    key={index}
+                    key={i}
                     className="result"
                 >
-                    <h1 className="title">{name}</h1>
+                    <h1 className="title">
+                        {start}
+                        <span>{match}</span>
+                        {end}
+                    </h1>
                 </div>
             );
         });
